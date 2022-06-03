bd0197ac49395bc090c496eae599a8ff045602e7
Ahn <yunjessicaahn@gmail.com>
2022-06-03 13:12:01 -0700

    passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -15,24 +15,6 @@ function sumAll (Num1, Num2) {
 			}
 		} return finalSum;
 }
-// function sumAll (num1, num2) {
-// 	let finalSum = 0;
-// 	let Num1 = Number(num1);
-// 	let Num2 = Number(num2);
-// 	if (Num1<Num2) {
-// 			for (let i = Num1; i <=Num2; i++) {
-// 		// console.log(i);
-// 			finalSum += i;
-// 			}
-// 		} else if (Num1>Num2) {
-// 			for (let i= Num2; i<=Num1; i++) {
-// 				finalSum += i;
-// 			}
-// 		} else {
-// 				finalSum = "ERROR"
-// 			} return finalSum;
-// 		}
-// Your issue likely is where you declare 'finalSum'. Notice that if you run it multiple times it will keep adding and adding. 
 
 // Do not edit below this line
 module.exports = sumAll;
