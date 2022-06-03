5a2e9f60ace7f1f035cd10a07a60ae07021094f3
Ahn <yunjessicaahn@gmail.com>
2022-06-03 13:49:22 -0700

    passed upto test3

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
+const leapYears = function (year) {
+// check if year can be divided by 4 but not by 100
+	if (year%4 == 0 && year%100 !==0) {
+		return true;
+	} else {
+		return false
+	}
+}
 
-};
-
+console.log (leapYears(1900));
 // Do not edit below this line
 module.exports = leapYears;
