2115c0e91be7a75bde434addee6e0cc8a971936c
Ahn <yunjessicaahn@gmail.com>
2022-06-03 19:53:45 -0700

    passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const ftoc = function() {
+// Fehrenheit to Celsius: subtract 32 and multiply by . 5556
+const ftoc = function(temF) {
+  let resultInCel = (temF-32) * 0.5556;
+//  then round to one decimal using toFixed
+  let roundResult = resultInCel.toFixed(1);
+  return Number(roundResult);
+}
 
-};
+// console.log (ftoc(-100));
+// console.log(typeof(ftoc(-100)));
 
-const ctof = function() {
-
-};
-
+// ctof(0) // celsius to fahrenheit, should return 32
+const ctof = function (temC) {
+  let resultInFeh = temC * 1.8 +32;
+  //  then round to one decimal using toFixed
+  let roundResult = resultInFeh.toFixed(1);
+  return Number(roundResult);
+}
 // Do not edit below this line
 module.exports = {
   ftoc,
